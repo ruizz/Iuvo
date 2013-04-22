3a452bb123d3a3b1df593163ebb1097ed99017c5
Curtis Wendel <cwendel16834@gmail.com>
2013-04-22 16:16:25 -0500

    added __unicode__ to all models

--- planner/admin.py
+++ planner/admin.py
@@ -1,12 +1,5 @@
 from django.contrib import admin
-from planner.models import UserAccount
-from planner.models import PersonalInfo
-from planner.models import DegreePlan
-from planner.models import CourseGroup
-from planner.models import CourseChoice
-from planner.models import DegreeSchedule
-from planner.models import Semester
-from planner.models import Course
+from planner.models import UserAccount, PersonalInfo, DegreePlan, CourseGroup, CourseChoice, DegreeSchedule, Semester, Course
 
 admin.site.register(UserAccount)
 admin.site.register(PersonalInfo)
--- planner/models.py
+++ planner/models.py
@@ -3,30 +3,37 @@ from django.db import models
 	
 class UserAccount(models.Model):
 	#One Personal Info, Degree plan, and degree schedule implicit
-	pass
+	def __unicode__(self):
+		return self.personlInfo.lastName + ", " + self.personlInfo.firstName
 
 class PersonalInfo(models.Model):
 	#FacebookData needs to be implimented
 	#
-	userAccount = models.OneToOneField(UserAccount)
+	userAccount = models.OneToOneField(UserAccount, related_name='personlInfo')
 	firstName = models.CharField(max_length=200)
 	lastName = models.CharField(max_length=200)
 	username = models.CharField(max_length=200)
 	school = models.CharField(max_length=200)
 
 	def __unicode__(self):
-		return self.lastName 
+		return self.lastName + ", " + self.firstName
 
 class DegreePlan(models.Model):
 	#Many courseGroups implicit
-	userAccount = models.OneToOneField(UserAccount)
+	userAccount = models.OneToOneField(UserAccount, related_name='degreePlan')
 	name = models.CharField(max_length=200)
 	major = models.CharField(max_length=200)
 
+	def __unicode__(self):
+		return self.name
+
 class CourseGroup(models.Model):
 	#Many CourseChoice implicit
-	degreePlan = models.ForeignKey(DegreePlan)
+	degreePlan = models.ForeignKey(DegreePlan, related_name='courseGroups')
 	name = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.name
 
 class CourseChoice(models.Model): #dynamic course, choose these
 	#Many Courses impicit, or just one
@@ -34,10 +41,20 @@ class CourseChoice(models.Model): #dynamic course, choose these
 	courseGroup = models.ForeignKey(CourseGroup, related_name='courseChoices')
 	courseSelected = models.BooleanField(default=False)
 	selectedCourse = models.ForeignKey('Course')
+
+	def __unicode__(self):
+		if courseSelected:
+			sel = selectedCourse.__unicode__()
+		else :
+			sel = "no selection"
+		return self.name + "(" + sel + ")"
 	
 class DegreeSchedule(models.Model): 
 	# many Semesters implicit
-	userAccount = models.OneToOneField(UserAccount)
+	userAccount = models.OneToOneField(UserAccount, related_name='degreeSchedule')
+
+	def __unicode__(self):
+		return userAccount.personlInfo.lastName
 
 class Semester(models.Model): 
 	degreeSched = models.ForeignKey(DegreeSchedule, related_name='semesters')
@@ -47,6 +64,7 @@ class Semester(models.Model):
 
 	def __unicode__(self):
 		return self.term + " " + str(self.year)
+
 class Course(models.Model): #static course, you have to take this
 	courseChoices = models.ManyToManyField(CourseChoice, related_name='courses', blank=True)
 	semesters = models.ManyToManyField(Semester, related_name='courses', blank=True)
