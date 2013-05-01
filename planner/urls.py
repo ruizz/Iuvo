01519914da1ecdc55eb747157cea361a02d62068
ruizz <ruiz.akpan@gmail.com>
2013-05-01 14:26:02 -0700

    Template code is now more modular. Updated views. Added more views. Other miscellaneous work.

--- planner/urls.py
+++ planner/urls.py
@@ -4,9 +4,16 @@ from planner import views
 urlpatterns = patterns('',
 	# ex: /
 	url(r'^$', views.index, name='index'),
+	
 	# ex: /user/john.doe/
-	url(r'^user/(?P<username>[A-Za-z0-9.-_]+)/account/$', 
-		views.userAccountView, name='userAccount'),
+	url(r'^user/(?P<username>[A-Za-z0-9.-_]+)/dashboard/$', views.dashboardView, name='dashboard'),
+	
+	# ex: /user/john.doe/
+	url(r'^user/(?P<username>[A-Za-z0-9.-_]+)/account/$', views.userAccountView, name='userAccount'),
+		
 	# ex: /user/john.doe/degreeplan/
 	url(r'^user/(?P<username>[A-Za-z0-9.-_]+)/degreeplan/$', views.degreePlanView, name='degreePlan'),
+	
+	# ex: /user/john.doe/schedule/
+	url(r'^user/(?P<username>[A-Za-z0-9.-_]+)/schedule/$', views.scheduleView, name='schedule'),
 )
--- planner/views.py
+++ planner/views.py
@@ -5,17 +5,27 @@ from planner.models import *
 def index(request):
 	userAccounts = UserAccount.objects.all();
 	context = { 'userAccounts': userAccounts}
-	return render(request, 'planner/index.html', context)
+	return render(request, 'planner/base_login.html', context)
 
+def dashboardView(request, username):
+	userAccount = get_object_or_404(UserAccount, username=username)
+	degreePlan = userAccount.degreePlan
+	context = { 'userAccount': userAccount, 'degreePlan': degreePlan}
+	return render(request, 'planner/base_myDashboard.html', context)
+	
 def userAccountView(request, username):
-
 	userAccount = get_object_or_404(UserAccount, username=username)
 	context = {	'userAccount': userAccount	}
-	
-	return render(request, 'planner/userAccount.html', context)
+	return render(request, 'planner/base_myAccount.html', context)
 
 def degreePlanView(request, username):
 	userAccount = get_object_or_404(UserAccount, username=username)
 	degreePlan = userAccount.degreePlan
 	context = { 'userAccount': userAccount, 'degreePlan': degreePlan}
-	return render(request, 'planner/degreePlan.html', context)
+	return render(request, 'planner/base_myDegreePlan.html', context)
+	
+def scheduleView(request, username):
+	userAccount = get_object_or_404(UserAccount, username=username)
+	degreePlan = userAccount.degreePlan
+	context = { 'userAccount': userAccount, 'degreePlan': degreePlan}
+	return render(request, 'planner/base_mySchedule.html', context)
